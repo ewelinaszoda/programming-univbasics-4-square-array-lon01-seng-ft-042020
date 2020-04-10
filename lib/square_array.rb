00ef861e2183128b4a786babb5ee00e08e942160
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-10 22:12:00 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+	squared_array = []
+	number = 0
+	while array[number] do 
+		squared_array << array[number]**2
+		number = number + 1
+	end 
+		squared_array
 end
+
+=begin
+def square_array(array)
+	squared_array = []
+	array.each {|value| squared_array << value ** 2}
+	squared_array
+end 
+=end
