b036c1ce920496a6a72a969f1e18e70ac3900550
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-10 21:47:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   square_array = []
   number = 0
-  while array(number) do 
-    square_array << array(number)**2
+  while array[number] do 
+    square_array << array[number]**2
   end
   number = number + 1
   square_array
